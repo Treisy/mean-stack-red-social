631423ad1d92e0ed8a0fe290dd5b44eec09adf8f
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-01-12 10:20:32 -0600

    Create the register user functionality in BE

--- README.md
+++ README.md
@@ -7,24 +7,29 @@ Versiones
 
 
 
-Install MongoDB
-     Execute:
-      brew update
-      brew install mongodb
-      brew tap homebrew/services
-      brew services start mongodb
-      brew serveces restart mongodb
+ #Install MongoDB Execute:
+   brew update
+   brew install mongodb
+   brew tap homebrew/services
+   brew services start mongodb
 
-Create the folder
-    $ sudo mkdir -p /data/db
-    $ whoami
-    user
-    $ sudo chown user /data/db
+#Create the folder
+   $ sudo mkdir -p /data/db
+   $ whoami
+   user
+   $ sudo chown user /data/db
 
-Add MongoDB to PATH
+#Add MongoDB to PATH
     export MONGO_PATH=/usr/local/mongodb
     export PATH=$PATH:$MONGO_PATH/bin
 
-For execute Mongo Shell use mongo
+#For execute Mongo Shell use 
+    mongo
 
-For run use npm start
+#For run use 
+    npm start
+
+#For start
+    brew services restart mongodb
+    mongo
+    
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 function home(req, res) {
@@ -17,7 +18,41 @@ function pruebas(req, res) {
     });
 }
 
+function saveUser(request, response) {
+    var params = request.body;
+    var user = new User();
+
+    if (params.name && params.surname && params.nick && params.email && params.password) {
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        bcrypt.hash(params.password, null, null, (err, hash) => {
+            user.password = hash;
+
+            user.save((err, userStored) => {
+                if(err) return response.status(500).send({message: 'Error al guardar usuario'});
+
+                if(userStored) {
+                    response.status(200).send({user: userStored});
+                } else {
+                    response.status(404).send({message: 'No se ha registrado el usuario' });
+                }
+            });
+        });
+
+    } else {
+        response.status(200).send({
+            message: 'Envia todos los campos necesarios'
+        });
+    }
+}
+
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 };
